8ffda6715dc062debe5b4475a29a6c04dcd7011b
Kunto Aji. K <kunto@sleekr.co>
2018-08-08 13:41:17 +0700

    add nvm installation

--- mac.sh
+++ mac.sh
@@ -39,6 +39,7 @@ color_echo "\nInstalling Graphviz"
 brew install graphviz
 
 color_echo "\nInstalling NVM Node.js LTS"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
